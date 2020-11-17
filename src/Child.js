4cc46a0f2fa1eb945ce87306b834593ee370cc9e
Justin_Webb <Webb.n.justin@gmail.com>
2020-11-16 23:27:28 -0600

    practice sending information to and from a parent

--- src/Child.js
+++ src/Child.js
@@ -1,12 +1,20 @@
+// For information to propagate down the component tree, parents pass props to their children
+// For information to propagate up the component tree, we typically invoke callbacks that were passed from parents to children as props
+// Components of the same level (sibling components) cannot communicate directly! We can only communicate up and down the component tree. So if multiple components need to share the same information, that state should live in the parent (or a more general ancestor) component.
+
 import React, { Component } from 'react'
 import { getRandomColor } from './randomColorGenerator.js'
 
+// pass handleColorChange and childColor props into a React event handler.
+
 class Child extends Component {
   render() {
+    console.log(this.props)
+    // function that invokes this.props.handleColorChange and passes in a random color as the argument
     return (
-      <div
+      <div onClick={this.props.handleColorChange(getRandomColor())}
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.props.color}}
       ></div>
     )
   }
--- src/Parent.js
+++ src/Parent.js
@@ -2,20 +2,35 @@ import React, { Component } from 'react'
 import { getRandomColor } from './randomColorGenerator.js'
 import Child from './Child'
 
+// Now, we could put some state in our Child component to keep track of its color. However, React components cannot pass data between 'sibling' components. Data can only flow up and down between parent/child. So if we update the color of one Child component, we have no way to pass that data to the other Child component. The solution is to store the color of the Child in the state of the Parent component. Then, we let the Parent component handle the passing of that data to each of it's children components.
+
 class Parent extends Component {
 
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: '#FFF'
     }
   }
+  // set the state in an instance method
+  // change not just the color state, but also the childrenColor. To practice sending data back to the parent, let's change our changeColor to take in an argument of newChildColor.
+
+  changeColor = (newChildColor) => {
+    this.setState({
+      color: getRandomColor(),
+      childrenColor: newChildColor
+    })
+  }
+
+  // To run this changeColor() method when either Child component is clicked pass this state changing function as a prop to both Child components.
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
+
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
       </div>
     )
   }
